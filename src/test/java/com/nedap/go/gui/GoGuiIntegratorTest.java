27587b5c769d221d6c6c98a2919b66a91e4368e8
AlexandrosKakotaritis <alexandros.kakotaritis@nedap.com>
2024-01-19 14:26:49 +0100

    Changed junit imports for used varsion

--- src/test/java/com/nedap/go/gui/GoGuiIntegratorTest.java
+++ src/test/java/com/nedap/go/gui/GoGuiIntegratorTest.java
@@ -1,8 +1,10 @@
 package com.nedap.go.gui;
 
-import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Example test class
