2875e6bbedb253fe6e9972572147ad831f57ab09
AlexandrosKakotaritis <alexandros.kakotaritis@nedap.com>
2024-01-23 12:01:00 +0100

    Delinting

--- src/test/java/com/nedap/go/model/BoardTest.java
+++ src/test/java/com/nedap/go/model/BoardTest.java
@@ -5,18 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
+
   private Board board;
-  @BeforeEach
-  public void setUp(){
-    board = new Board();
-  }
 
   private static boolean containsArray(List<List<Integer>> listOfLists, Integer[] arrayToCheck) {
     for (List<Integer> list : listOfLists) {
@@ -27,14 +23,19 @@ public class BoardTest {
     return false;
   }
 
+  @BeforeEach
+  public void setUp() {
+    board = new Board();
+  }
+
   @Test
   public void testIsField() {
-    assertTrue(board.isField(board.getDim()/2));
+    assertTrue(board.isField(board.getDim() / 2));
     assertFalse(board.isField(board.getDim() * board.getDim() + 1));
 
-    assertTrue(board.isField(1,1));
+    assertTrue(board.isField(1, 1));
     assertFalse(board.isField(1, board.getDim() + 2));
-    assertFalse(board.isField(-1, board.getDim()-1));
+    assertFalse(board.isField(-1, board.getDim() - 1));
   }
 
   @Test
@@ -51,6 +52,7 @@ public class BoardTest {
     board.setField(board.getDim() + 1, Stone.EMPTY);
     assertEquals(Stone.BLACK, board.getField(board.getDim() + 1));
   }
+
   @Test
   public void testSetFieldXY() {
     for (int i = 0; i < board.getDim(); i++) {
@@ -63,23 +65,23 @@ public class BoardTest {
 
     }
 
-    board.setField(0, board.getDim() -1, Stone.WHITE);
-    assertEquals(Stone.BLACK, board.getField(0, board.getDim() -1));
+    board.setField(0, board.getDim() - 1, Stone.WHITE);
+    assertEquals(Stone.BLACK, board.getField(0, board.getDim() - 1));
 
-    board.setField(2, board.getDim() -1, Stone.EMPTY);
-    assertEquals(Stone.BLACK, board.getField(2, board.getDim() -1));
+    board.setField(2, board.getDim() - 1, Stone.EMPTY);
+    assertEquals(Stone.BLACK, board.getField(2, board.getDim() - 1));
   }
 
   @Test
-  public void testGetStoneChains(){
+  public void testGetStoneChains() {
     Integer[] black = new Integer[]{7, 16, 25, 34, 43, 52, 61, 70, 79};
     Integer[] white = new Integer[]{20, 21, 22, 23, 29, 32, 38, 41, 47, 48, 49, 50};
 
-    for (int i = 0; i < black.length; i++) {
-      board.setField(black[i], Stone.BLACK);
+    for (Integer integer : black) {
+      board.setField(integer, Stone.BLACK);
     }
-    for (int i = 0; i < white.length; i++) {
-      board.setField(white[i], Stone.WHITE);
+    for (Integer integer : white) {
+      board.setField(integer, Stone.WHITE);
     }
 
     board.setField(80, Stone.WHITE);
@@ -99,23 +101,22 @@ public class BoardTest {
     board.reset();
 
     int[] blackSquare = new int[]{11, 12, 13, 19, 23, 28, 32, 37, 41, 47, 48, 49};
-    for (int i = 0; i < blackSquare.length; i++) {
-      board.setField(blackSquare[i], Stone.BLACK);
+    for (int index : blackSquare) {
+      board.setField(index, Stone.BLACK);
     }
     board.setField(25, Stone.WHITE);
     System.out.println(board);
     assertEquals(21, board.getScore(Stone.BLACK));
     board.reset();
 
-
     int[] black = new int[]{21, 22, 23, 29, 33, 37, 38, 42, 46, 51, 56, 57, 58, 59};
     int[] white = new int[]{12, 13, 14, 28, 66, 67, 68, 7, 16, 25, 34, 43, 52, 61, 70, 79};
 
-    for (int i = 0; i < black.length; i++) {
-      board.setField(black[i], Stone.BLACK);
+    for (int index : black) {
+      board.setField(index, Stone.BLACK);
     }
-    for (int i = 0; i < white.length; i++) {
-      board.setField(white[i], Stone.WHITE);
+    for (int index : white) {
+      board.setField(index, Stone.WHITE);
     }
 
     System.out.println(board);
@@ -124,15 +125,15 @@ public class BoardTest {
   }
 
   @Test
-  public void testCapture(){
+  public void testCapture() {
     int[] black = new int[]{21, 22, 23, 29, 33, 39, 43, 49, 53, 58, 61, 68, 69};
     int[] white = new int[]{30, 31, 32, 40, 41, 42, 50, 51, 52, 59, 60};
 
-    for (int i = 0; i < black.length; i++) {
-      board.setField(black[i], Stone.BLACK);
+    for (int index : black) {
+      board.setField(index, Stone.BLACK);
     }
-    for (int i = 0; i < white.length; i++) {
-      board.setField(white[i], Stone.WHITE);
+    for (int index : white) {
+      board.setField(index, Stone.WHITE);
     }
     System.out.println(board);
     board.calculateCaptures(Stone.BLACK);
@@ -142,15 +143,15 @@ public class BoardTest {
   }
 
   @Test
-  public void testEdgeCapture(){
+  public void testEdgeCapture() {
     int[] black = new int[]{1, 10, 19, 27, 28};
     int[] white = new int[]{0, 9, 18};
 
-    for (int i = 0; i < black.length; i++) {
-      board.setField(black[i], Stone.BLACK);
+    for (int index : black) {
+      board.setField(index, Stone.BLACK);
     }
-    for (int i = 0; i < white.length; i++) {
-      board.setField(white[i], Stone.WHITE);
+    for (int index : white) {
+      board.setField(index, Stone.WHITE);
     }
     String boardBeforeCapture = board.toString();
     board.calculateCaptures(Stone.BLACK);
@@ -159,26 +160,27 @@ public class BoardTest {
     assertEquals(81, board.getScore(Stone.BLACK));
 
   }
+
   @Test
-  public void testDeepCopy(){
+  public void testDeepCopy() {
     int[] black = new int[]{21, 22, 23, 29, 33, 37, 38, 42, 46, 51, 56, 57, 58, 59};
     int[] white = new int[]{12, 13, 14, 28, 66, 67, 68, 7, 16, 25, 34, 43, 52, 61, 70, 79};
-    for (int i = 0; i < black.length; i++) {
-      board.setField(black[i], Stone.BLACK);
+    for (int index : black) {
+      board.setField(index, Stone.BLACK);
     }
-    for (int i = 0; i < white.length; i++) {
-      board.setField(white[i], Stone.WHITE);
+    for (int index : white) {
+      board.setField(index, Stone.WHITE);
     }
     Board copiedBoard = board.deepCopy();
 
     assertEquals(board.toString(), copiedBoard.toString());
-//    assertEquals(board.getScore(Stone.BLACK), copiedBoard.getScore(Stone.BLACK));
-//    assertEquals(board.getScore(Stone.WHITE), copiedBoard.getScore(Stone.WHITE));
+    assertEquals(board.getScore(Stone.BLACK), copiedBoard.getScore(Stone.BLACK));
+    assertEquals(board.getScore(Stone.WHITE), copiedBoard.getScore(Stone.WHITE));
 
     board.setField(0, Stone.BLACK);
 
     assertNotEquals(board.toString(), copiedBoard.toString());
-//    assertNotEquals(board.getScore(Stone.BLACK), copiedBoard.getScore(Stone.BLACK));
-//    assertNotEquals(board.getScore(Stone.WHITE), copiedBoard.getScore(Stone.WHITE));
+    assertNotEquals(board.getScore(Stone.BLACK), copiedBoard.getScore(Stone.BLACK));
+    assertEquals(board.getScore(Stone.WHITE), copiedBoard.getScore(Stone.WHITE));
   }
 }
