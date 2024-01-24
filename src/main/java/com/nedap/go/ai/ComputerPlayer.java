507c85d2fe529ea87ffc7c0631065fd20c409980
AlexandrosKakotaritis <alexandros.kakotaritis@nedap.com>
2024-01-24 17:46:11 +0100

    Added JavaDoc

--- src/main/java/com/nedap/go/ai/ComputerPlayer.java
+++ src/main/java/com/nedap/go/ai/ComputerPlayer.java
@@ -6,38 +6,63 @@ import com.nedap.go.model.Game;
 import com.nedap.go.model.Move;
 import com.nedap.go.model.Stone;
 
+/**
+ * Class for AI players.
+ */
 public class ComputerPlayer extends AbstractPlayer {
 
-    Stone stone;
+  Stone stone;
 
-    Strategy strategy;
+  Strategy strategy;
 
-    public ComputerPlayer(Strategy strategy, Stone stone){
-        super(strategy.getName() + "-" + stone);
-        this.strategy = strategy;
-    }
+  /**
+   * Constructs the ComputerPlayer with the desired stone and strategy.
+   *
+   * @param strategy The desired strategy.
+   * @param stone    The desired stone.
+   */
+  public ComputerPlayer(Strategy strategy, Stone stone) {
+    super(strategy.getName() + "-" + stone);
+    this.strategy = strategy;
+    this.stone = stone;
+  }
 
-    public Strategy getStrategy() {
-        return strategy;
-    }
+  /**
+   * Getter for the strategy.
+   *
+   * @return The strategy of the computer player.
+   */
+  public Strategy getStrategy() {
+    return strategy;
+  }
 
-    public void setStrategy(Strategy strategy){
-        this.strategy = strategy;
-    }
+  /**
+   * Setter for the strategy.
+   *
+   * @param strategy The strategy to use.
+   */
+  public void setStrategy(Strategy strategy) {
+    this.strategy = strategy;
+  }
 
-    /**
-     * Determines the next move, if the game still has available moves.
-     *
-     * @param game the current game
-     * @return the player's choice
-     */
-    @Override
-    public Move determineMove(Game game) {
-        return strategy.determineMove(game);
+  /**
+   * Determines the next move, if the game still has available moves.
+   *
+   * @param game the current game
+   * @return the player's choice
+   */
+  @Override
+  public Move determineMove(Game game) {
+    return strategy.determineMove(game);
 
-    }
+  }
 
-    public Stone getStone() {
-        return stone;
-    }
+  /**
+   * Return the stone of the player.
+   *
+   * @return The color of the stone.
+   */
+  public Stone getStone() {
+    return stone;
+  }
 }
