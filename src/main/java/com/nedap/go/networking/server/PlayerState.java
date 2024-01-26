d94a373a1079d5d52793815bfd66a2a436094faf
AlexandrosKakotaritis <alexandros.kakotaritis@nedap.com>
2024-01-26 21:37:52 +0100

    Added javadoc

--- src/main/java/com/nedap/go/networking/server/PlayerState.java
+++ src/main/java/com/nedap/go/networking/server/PlayerState.java
@@ -1,5 +1,9 @@
 package com.nedap.go.networking.server;
 
+/**
+ * A player can be in 4 distinct states. Useful for ignoring
+ * incoming messages not appropriate for each state
+ */
 public enum PlayerState {
-    FRESH, PREGAME, INQUEUE, INGAME
+    FRESH, PREGAME, IN_QUEUE, IN_GAME
 }
