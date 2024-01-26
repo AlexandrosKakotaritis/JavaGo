1f88adb2743802adc5f6a4f47777c6ccee45c1bb
AlexandrosKakotaritis <alexandros.kakotaritis@nedap.com>
2024-01-26 21:38:04 +0100

    Typo on default message

--- src/main/java/com/nedap/go/networking/server/NotYourTurnException.java
+++ src/main/java/com/nedap/go/networking/server/NotYourTurnException.java
@@ -2,7 +2,7 @@ package com.nedap.go.networking.server;
 
 public class NotYourTurnException extends Exception {
   public NotYourTurnException(){
-    super("It's no your turn!");
+    super("It's not your turn!");
   }
   public NotYourTurnException(String message){
     super(message);
